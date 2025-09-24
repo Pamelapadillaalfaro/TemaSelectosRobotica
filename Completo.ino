c10d3e29b4343cbf44725e31604e8da706df7b05
Nri~k <113216104+Nrikuik@users.noreply.github.com>
2025-09-24 16:33:51 -0600

    Add files via upload

--- Completo.ino
+++ Completo.ino
@@ -5,13 +5,20 @@ const int analogOutPin = 9; // Analog output pin that the LED is attached to
 const int contactPin =3;
 const int tempPin =A2;
 const int photoDiPin =7;
+const int pinPWM=10;
+
+
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
+int valorPWM;
 String option;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(comparadorPin, INPUT);
+  //Pin para el PWM
+  pinMode(pinPWM, OUTPUT);
+  pinMode(photoDiPin, INPUT);
 }
 
 void loop() {
@@ -65,17 +72,51 @@ void loop() {
     }
     //Contacto
     else if(option.equals("shs photord")) {
-      //Serial.print("Valor = ");
-      // put your main code here, to run repeatedly:
-      // read the analog in value:
+      analogWrite(pinPWM, valorPWM);
       sensorValue = digitalRead(photoDiPin);
-      // map it to the range of the analog out:
-      // change the analog out value:
       Serial.println(sensorValue);
     }
+    else if(option.equals("shs calibrator")) {
+      //Serial.print("Calibrabdo");
+      valorPWM = funcionCalibrar();
+      Serial.println("Finish Calibration");
+      }
     else{
      Serial.println("Comando no disponible");
     }
   }
   delay(1000);
+}
+
+int funcionCalibrar(){
+
+  int cicloTrab=0;
+  int incremento=5;
+  int photoDiPin=7;
+  int sensorvalue=0;
+  int contador=0;
+  int cicloFijo=0;
+
+  // put your main code here, to run repeatedly
+  while (contador<=1) {
+  analogWrite(pinPWM, cicloTrab);
+  cicloTrab=cicloTrab+incremento;
+  
+  if (cicloTrab <= 0 || cicloTrab>=255){
+    incremento=-incremento;
+    }
+//  Serial.print("CICLO DE TRABAJO: ");
+//  Serial.println(cicloTrab);
+  sensorvalue=digitalRead(photoDiPin);
+  if (sensorvalue==0){
+    contador=contador+1;
+    }
+  delay(300);
+  }cicloFijo=cicloTrab-5;
+  
+//  Serial.print("CICLO DE TRABAJO FINAL: ");
+//  Serial.println(cicloFijo);
+//  Serial.print("VALOR COMPARADO: ");
+
+  return cicloFijo;
 }
