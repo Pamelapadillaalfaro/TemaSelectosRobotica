ef9f922d348b9db6a9bc3f4910782d917c3d6c2b
Nri~k <113216104+Nrikuik@users.noreply.github.com>
2025-10-02 17:38:37 -0600

    Add files via upload
    Practica 3 Motores parte 1 4 señales digitales

---
+++ P3_Motores.ino
@@ -0,0 +1,72 @@
+//const int analogInPin = A0;  // Analog input pin
+const int speed_1Pin = 2;
+const int speed_2Pin = 4;
+const int direction_1Pin = 7; // Digital output pin
+const int direction_2Pin = 8; // Digital output pin
+
+int speedValue = 0;        
+int directionValue = 0;
+String option;              // instructioón value
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  // pinMode
+  pinMode(speed_1Pin, OUTPUT);
+  pinMode(direction_1Pin, OUTPUT);
+  pinMode(speed_2Pin, OUTPUT);
+  pinMode(direction_2Pin, OUTPUT);
+}
+
+void loop() {
+ int directionPin;
+ int speedPin;
+  if (Serial.available()>0){
+    // Read instruction ...
+    option=Serial.readStringUntil('\n');
+    //int posicion = cadena.indexOf(aBuscar) --- Find index position un String
+    if (option.indexOf("A1") >= 0) { //Motor A1 instruction
+      directionPin = direction_1Pin;
+      speedPin = speed_1Pin;
+      Serial.println("Motor A1");
+    }
+    else if(option.indexOf("A2") >= 0) { //Motor A2 instruction
+      directionPin = direction_2Pin;
+      speedPin = speed_2Pin;
+      Serial.println("Motor A2");
+    }
+
+    speedValue = on_offFunction(option);
+    directionValue = turnFunction(option);
+      
+    digitalWrite(directionPin, directionValue);
+    digitalWrite(speedPin, speedValue);
+
+    Serial.print("Direction:");
+    Serial.println(directionValue);
+    
+    Serial.print("ON / OFF:");
+    Serial.println(speedValue);
+    delay (2);
+  }     
+}
+
+int turnFunction(String instruct){
+    
+    if (instruct.indexOf("left") >= 0 ) {
+      return 1;
+    }
+    else if(instruct.indexOf("rigth") >= 0 ) {
+      return 0;
+    }
+  }
+  
+int on_offFunction(String instruct){
+    
+    if (instruct.indexOf("on") >= 0 ) {
+      return 1;
+    }
+    else if(instruct.indexOf("off") >= 0 ) {
+      return 0;
+    }
+  } 
